3731cf1299df77eb8c22377e791a753cba390eda
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-12-17 15:40:16 +0100

    Adjusted visualization; updated .gitignore; updated Roxygen2.

--- R/control_ccf.R
+++ R/control_ccf.R
@@ -4,7 +4,7 @@
 rm(list = ls(all = TRUE))
 
 ## required packages
-lib <- c("gimms", "Rsenal", "doParallel", "RColorBrewer", "grid")
+lib <- c("gimms", "Rsenal", "doParallel", "RColorBrewer", "grid", "dplyr")
 Orcs::loadPkgs(lib)
 
 ## parallelization
@@ -144,30 +144,62 @@ dat_ccf <- foreach(i = 1:nrow(mat_chirps), .combine = "rbind",
 }
 
 saveRDS(dat_ccf, "data/ccf.rds")
-dat_ccf <- loadRDS("data/ccf.rds")
+dat_ccf <- readRDS("data/ccf.rds")
+
+## limit to lag = [0;2] and identify maximum r, minimum p
+dat_ccf %>%
+  filter(lag != 3 | is.na(lag)) %>%
+  group_by(cell, month) %>%
+  filter(abs(r) == max(abs(r)) | is.na(r)) -> dat_ccf_r
+
+dat_ccf %>%
+  filter(lag != 3 | is.na(lag)) %>%
+  group_by(cell, month) %>%
+  filter(p == min(p) | is.na(p)) -> dat_ccf_p
 
 ## insert values
-rst_lag <- rst_cor <- rst_chirps[[1]]
+rst_lag <- rst_cor <- raster("inst/templates/template_chirps.tif")
 rst_lag[] <- rst_cor[] <- NA
 
 ## colors
 cols <- colorRampPalette(brewer.pal(9, "RdBu"))
-cols_lag <- brewer.pal(9, "YlOrRd")[c(1, 4, 6, 9)]
+cols_lag <- brewer.pal(9, "YlOrRd")[c(3, 5, 8)]
 
 lst_ccf <- foreach(i = 1:12, month = month.abb, .packages = "Rsenal") %dopar% {
-  id_month <- seq(i, nrow(dat_ccf), 12)
+  id_month <- seq(i, nrow(dat_ccf_r), 12)
 
-  rst_cor_tmp <- raster::setValues(rst_cor, dat_ccf$r[id_month])
+  dat <- dat_ccf_r[id_month, ]
+  if (any(duplicated(dat$cell)))
+    dat <- dat[-which(duplicated(dat$cell)), ]
+
+  rst_cor_tmp <- raster::setValues(rst_cor, dat$r)
+
+  # reject non-significant pixels
+  rst_p_tmp <- raster::setValues(rst_cor, dat$p)
+  rst_cor_tmp <- raster::overlay(rst_cor_tmp, rst_p_tmp, fun = function(x, y) {
+    x[y[] >= 0.05] <- NA
+    return(x)
+  })
+
   p_cor_tmp <- sp::spplot(rst_cor_tmp, scales = list(draw = TRUE, cex = .7),
                           at = seq(-1, 1, .01), col.regions = cols(1000),
                           colorkey = list(space = "top", width = .5, height = .5),
                           main = list("Pearson's r", cex = .8),
                           sp.layout = list("sp.text", loc = c(45.5, 25.85),
-                                           txt = month, font = 2, cex = .7)) +
+                                           txt = month, font = 2, cex = .7),
+                          xlab = list("Longitude", cex = .8),
+                          ylab = list("Latitude", cex = .8)) +
     latticeExtra::layer(sp.polygons(spy_iran))
 
 
-  rst_lag_tmp <- raster::setValues(rst_lag, dat_ccf$lag[id_month])
+  rst_lag_tmp <- raster::setValues(rst_lag, dat$lag)
+
+  # reject non-significant pixels
+  rst_lag_tmp <- raster::overlay(rst_lag_tmp, rst_p_tmp, fun = function(x, y) {
+    x[y[] >= 0.05] <- NA
+    return(x)
+  })
+
   rst_lag_tmp <- raster::ratify(rst_lag_tmp)
   rat_lag_tmp <- levels(rst_lag_tmp)[[1]]
   levels(rst_lag_tmp) <- rat_lag_tmp
@@ -212,42 +244,6 @@ p_lag <- latticeCombineGrid(lst_p_lag, layout = c(4, 3))
 png(paste0(dir_extdata_chirps, "/vis/ccf_lag.png"), width = 20, height = 16,
     units = "cm", res = 500)
 print(p_lag)
-dev.off()
-
-## create figure
-cols_acf <- colorRampPalette(rev(brewer.pal(5, "RdGy")))
-p_acf <- spplot(rst_acf, scales = list(draw = TRUE),
-                at = seq(-.65, .65, .01), col.regions = cols_acf(1000),
-                colorkey = list(space = "top", width = .75, height = 1),
-                main = "r\n") +
-  latticeExtra::layer(sp.polygons(spy_iran))
-
-cols_lag <- colorRampPalette(brewer.pal(5, "YlGn"))
-p_lag <- spplot(rst_lag, scales = list(draw = TRUE),
-                at = seq(-.5, 6.5, 1), col.regions = envinmrPalette(7)) +
-  latticeExtra::layer(sp.polygons(spy_iran))
-
-p <- latticeCombineGrid(list(p_acf, p_lag), layout = c(1, 2))
-
-png("data/vis/ccf.png", width = 14, height = 20, units = "cm", res = 500)
-grid.newpage()
-vp0 <- viewport(x = 0, y = .1,
-                height = .9, width = 1,
-                just = c("left", "bottom"),
-                name = "fig.vp")
-pushViewport(vp0)
-print(p, newpage = FALSE)
-
-vp1 <- viewport(x = .5, y = -.06,
-                height = 0.1, width = .75,
-                just = c("center", "bottom"),
-                name = "key.vp")
-pushViewport(vp1)
-lattice::draw.colorkey(key = list(col = envinmrPalette(7), width = .75, height = .75,
-                                  at = 0:7, space = "bottom"), draw = TRUE)
-grid.text("lag (1/2 months)", x = 0.5, y = -.1, just = c("centre", "bottom"),
-          gp = gpar(font = 2, cex = .85))
-
 dev.off()
 
 ## deregister parallel backend
