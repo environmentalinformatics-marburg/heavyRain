13b980cc9b380a8629572e4e6c60d666e67baa1e
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-05-08 17:18:44 +0200

    Update: enabled download of all different sub-products (i.e., 6-hourly, daily, pentad, dekad, monthly).

--- R/downloadChirps.R
+++ R/downloadChirps.R
@@ -5,31 +5,28 @@
 #' data for a given time span from Climate Hazards Group's (CHG) FTP server
 #' (\url{ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRPS-2.0/}).
 #'
-#' @param region 'character. Region of interest; one of 'global', 'whem'
+#' @param region 'character'. Region of interest; one of 'global', 'whem'
 #' (western hemisphere), 'africa' or 'camer-carib' (Central America and the
 #' Caribbean). See the official CHIRPS README (available online at
 #' \url{ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRPS-2.0/README-CHIRPS.txt})
 #' for further information.
-#' @param format 'character'. Desired file format; one of 'tifs', 'netcdf',
-#' 'bils' or 'pngs'.
+#' @param format 'character'. Desired file format; one of 'tifs', 'bils' or
+#' 'pngs'.
 #' @param tres 'character'. Desired temporal resolution; one of 'daily',
 #' 'pentad', 'dekad' or 'monthly'.
 #' @param sres 'numeric'. Desired spatial resolution; one of
 #' \code{c(0.05, 0.25)}.
-#' @param begin 'date'. If not supplied, data download starts with the first
-#' record available.
-#' @param end 'date'. If not supplied, data download stops at the last record
-#' available.
+#' @param begin,end 'date'. If not supplied, data download starts (stops) with
+#' the first (last) record available.
 #' @param dsn 'character'. Destination folder for file download, defaults to the
 #' current working directory.
 #' @param overwrite Logical. If \code{TRUE}, already downloaded files in 'dsn'
 #' will be overwritten.
-#' @param cores 'integer'. The number of cores used for parallel download via
-#' \strong{doParallel}.
-#' @param ... Further arguments passed on to \code{\link{download.file}}.
+#' @param cores 'integer'. The number of cores used for parallel downloads.
+#' @param ... Further arguments passed to \code{\link{download.file}}.
 #'
 #' @return
-#' A vector of filepaths.
+#' A \code{character} vector of file paths.
 #'
 #' @author
 #' Florian Detsch
@@ -49,87 +46,72 @@
 NULL
 
 downloadChirps <- function(region = c("global", "whem", "africa", "camer-carib"),
-                           format = c("tifs", "netcdf"),
-                           tres = c("daily"),
+                           format = c("tifs", "bils", "pngs"),
+                           tres = c("daily", "pentad", "dekad", "monthly"),
                            sres = c(0.05, 0.25),
                            begin = NULL, end = NULL,
                            dsn = getwd(), overwrite = FALSE,
                            cores = 1L, ...) {
 
-  ## server address
-  ch_url <- "ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRPS-2.0"
+  ## time frame
+  if (is.null(begin)) begin <- as.Date("1981-01-01")
+  if (is.null(end)) end <- Sys.Date()
+
+  ## destination folder
+  if (!dir.exists(dsn)) dir.create(dsn, recursive = TRUE)
 
   ## dataset
   ch_ext <- paste(region[1], tres[1], sep = "_")
 
-  ## format
-  ch_ext <- paste(ch_ext, format[1], sep = "/")
-
-  ## spatial resolution
-  sres <- formatC(sres[1] * 100, width = 2, flag = 0)
-  sres <- paste0("p", sres, "/")
-  ch_ext <- paste(ch_ext, sres, sep = "/")
-
-  ## available years
-  ch_url <- paste(ch_url, ch_ext, sep = "/")
-
-  ch_yrs <- RCurl::getURL(ch_url, dirlistonly = TRUE)
-  ch_yrs <- unlist(strsplit(ch_yrs, "\n"))
-
-  ## available days
-  if (is.null(begin)) begin <- as.Date(paste0(ch_yrs[1], "-01-01"))
-  if (is.null(end)) end <- as.Date(paste0(ch_yrs[length(ch_yrs)], "-12-31"))
-
-  ## download year by year
-  if (cores > 1) {
-    cl <- parallel::makeCluster(cores)
-    doParallel::registerDoParallel(cl)
+  ## format (not required for 6-hourly files)
+  ch_ext <- if (tres == "6-hourly") {
+    paste0(ch_ext, "/p1_bin")
+  } else {
+    paste0(ch_ext, "/", format[1])
   }
 
-  lst_fls <- lapply(ch_yrs, function(i) {
+  ## spatial resolution (only required for daily files)
+  if (tres == "daily") {
+    sres <- formatC(sres[1] * 100, width = 2, flag = 0)
+    sres <- paste0("p", sres)
+    ch_ext <- paste0(ch_ext, "/", sres)
+  }
 
-    cat("Year", i, "is in. Initializing data download...\n")
-    ch_url_yr <- paste0(ch_url, i, "/")
+  ## retrieve file list
+  ch_url <- paste0(serverPath(), "/", ch_ext, "/")
 
-    if (i == substr(Sys.Date(), 1, 4)) {
+  onl <- if (tres %in% c("pentad", "dekad", "monthly")) {
+    lsCat1(ch_url)
+  } else if (tres == "6-hourly") {
+    lsCat2(ch_url, begin, end)
+  } else if (tres == "daily") {
+    lsCat3(ch_url, begin, end)
+  } else stop("Specified temporal resolution (currently) not available.\n")
 
-      ch_fls <- RCurl::getURL(ch_url_yr, dirlistonly = TRUE)
-      ch_fls <- unlist(strsplit(ch_fls, "\n"))
+  ## download files
+  cl <- parallel::makePSOCKcluster(cores)
+  on.exit(parallel::stopCluster(cl))
 
-      ch_dys <- substr(ch_fls, 13, 22)
-      dt_dys <- as.Date(ch_dys, format = "%Y.%m.%d")
+  dots <- list(...)
+  parallel::clusterExport(cl, c("dsn", "overwrite", "dots"),
+                          envir = environment())
 
-    } else {
-      dt_dys <- seq(as.Date(paste0(i, "-01-01")), # start
-                    as.Date(paste0(i, "-12-31")), # end
-                    1)                            # by
+  do.call("c", parallel::parLapply(cl, onl, function(j) {
+
+    # download current file
+    destfile <- paste0(normalizePath(dsn), "/", basename(j))
+    jnk <- if (!file.exists(destfile) | overwrite) {
+      dots_sub <- list(url = j, destfile = destfile)
+      dots_sub <- append(dots_sub, dots)
+
+      try(do.call(utils::download.file, args = dots_sub), silent = TRUE)
     }
 
-    suppressWarnings(
-      ch_fls <- foreach(j = dt_dys, .export = ls(envir = globalenv()),
-                        .combine = "c") %dopar% {
-
-        ch_dy <- strftime(j, format = "%Y.%m.%d")
-        ch_dy <- paste("chirps-v2.0", ch_dy, "tif.gz", sep = ".")
-        ch_url_dy <- paste0(ch_url_yr, ch_dy)
-
-        destfile <- paste(dsn, ch_dy, sep = "/")
-
-        if (!file.exists(destfile) | overwrite)
-          try(download.file(ch_url_dy, destfile, ...), silent = FALSE)
-
-        return(destfile)
-      }
-    )
-
-    return(ch_fls)
-  })
-
-  ## deregister parallel backend
-  if (cores > 1)
-    parallel::stopCluster(cl)
-
-  ## return files
-  ch_fls <- do.call("c", lst_fls)
-  return(ch_fls)
+    # if file download failed, return nothing, else return local file
+    if (inherits(jnk, "try-error")) {
+      return(invisible())
+    } else {
+      return(destfile)
+    }
+  }))
 }
---
+++ R/downloadControls.R
@@ -0,0 +1,79 @@
+### List inventory of CHIRPS pentad, dekad, and monthly data -----
+
+lsCat1 <- function(url) {
+  onl <- getSplitURL(url)
+  paste0(url, onl)
+}
+
+
+### List inventory of CHIRPS 6-hourly data -----
+
+lsCat2 <- function(url, begin, end) {
+
+  ## available months
+  mts <- getSplitURL(url)
+  mts <- mts[nchar(mts) == 6]
+
+  ## subset with months of interest
+  ids <- match(sapply(c(begin, end), function(i) format(i, "%Y%m")), mts)
+
+  if (any(is.na(ids))) {
+    if (is.na(ids[1])) ids[1] <- 1
+    if (is.na(ids[2])) ids[2] <- length(mts)
+  }
+
+  mts <- mts[ids[1]:ids[2]]
+  drs <- paste0(url, mts, "/")
+
+  ## available time steps
+  do.call("c", lapply(drs, function(i) {
+    fls <- getSplitURL(i)
+    paste0(i, fls)
+  }))
+}
+
+
+### List inventory of CHIRPS daily data -----
+
+lsCat3 <- function(url, begin, end) {
+
+  ## available years
+  yrs <- getSplitURL(url)
+  yrs <- yrs[nchar(yrs) == 4]
+
+  ## subset with years of interest
+  ids <- match(sapply(c(begin, end), function(i) format(i, "%Y")), yrs)
+
+  if (any(is.na(ids))) {
+    if (is.na(ids[1])) ids[1] <- 1
+    if (is.na(ids[2])) ids[2] <- length(yrs)
+  }
+
+  yrs <- yrs[ids[1]:ids[2]]
+  drs <- paste0(url, yrs, "/")
+
+  ## available days
+  do.call("c", lapply(drs, function(i) {
+    fls <- getSplitURL(i)
+    paste0(i, fls)
+  }))
+}
+
+
+### Server paths for CHIRPS data download -----
+
+serverPath <- function(server = "chirps", version = "2.0") {
+  if (server != "chirps")
+    stop("Specified server not available or (currently) not supported.\n")
+
+  if (!(version %in% c("2.0", "1.8")))
+    stop("Specified version not available or (currently) not supported.\n")
+
+  paste0("ftp://chg-ftpout.geog.ucsb.edu/pub/org/chg/products/CHIRPS-", version)
+}
+
+
+getSplitURL <- function(url) {
+  onl <- RCurl::getURL(url, dirlistonly = TRUE)
+  unlist(strsplit(onl, "\n"))
+}
