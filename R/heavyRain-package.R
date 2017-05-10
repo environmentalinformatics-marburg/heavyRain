04e2c5117d39bb1bd71d25dee049f86ebbb876f5
fdetsch <florian.detsch@staff.uni-marburg.de>
2017-05-10 15:49:55 +0200

    Update: package-related stuff.

---
+++ R/heavyRain-package.R
@@ -0,0 +1,14 @@
+#' Download and Pre-Process CHIRPS and TRMM Rainfall Data Sets in R
+#'
+#' This is a set of functions to download and pre-process CHIRPS and TRMM
+#' rainfall data sets in R.
+#'
+#' @name heavyRain-package
+#' @aliases heavyRainpackage
+#' @docType package
+#' @title Download and Pre-Process CHIRPS and TRMM Rainfall Data Sets in R
+#' @author Florian Detsch
+#'
+#' @keywords package
+#'
+NULL
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# chirps
+# heavyRain
+
+Download and pre-process (partly) satellite-based [CHIRPS](http://chg.geog.ucsb.edu/data/chirps/) and [TRMM](https://trmm.gsfc.nasa.gov/) rainfall data sets in R.
